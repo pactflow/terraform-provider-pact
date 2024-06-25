42559c4f7c2d6382b365354154f5dae74287a42e
Sauparna Gupta <sauparna.gupta@gmail.com>
2024-06-25 12:26:35 +0530

    chore: updated the trasnform script

--- scripts/transform-broken-keys.sh
+++ scripts/transform-broken-keys.sh
@@ -2,7 +2,7 @@
 
 set -e
 cat $PWD/client/pacts/terraform-client-pactflow-application-saas.json | grep $.header
-sed -i -E "s/\$\.header\./\$\.headers\./g" $PWD/client/pacts/terraform-client-pactflow-application-saas.json
+sed -i.bak -E "s/\$\.header\./\$\.headers\./g" "$PWD/client/pacts/terraform-client-pactflow-application-saas.json"
 cat $PWD/client/pacts/terraform-client-pactflow-application-saas.json | grep $.header
 
 echo "Done..."
