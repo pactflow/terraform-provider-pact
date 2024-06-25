8c734a11e443b2ec4a20c0a5d15b636132994ee3
Sauparna Gupta <sauparna.gupta@gmail.com>
2024-06-25 11:55:45 +0530

    chore: debugging the pact file transform script

--- scripts/transform-broken-keys.sh
+++ scripts/transform-broken-keys.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -e
-
+cat $PWD/client/pacts/terraform-client-pactflow-application-saas.json | grep $.header
 sed -i -E "s/\$\.header\./\$\.headers\./g" $PWD/client/pacts/terraform-client-pactflow-application-saas.json
+cat $PWD/client/pacts/terraform-client-pactflow-application-saas.json | grep $.header
 
 echo "Done..."
