b757beef9b789085bcf1b01c8d0e0f4b5e4aeaaf
Sauparna Gupta <sauparna.gupta@gmail.com>
2024-06-25 12:56:40 +0530

    chore: fixed the trasnform script for linux env

--- scripts/transform-broken-keys.sh
+++ scripts/transform-broken-keys.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 set -e
-cat $PWD/client/pacts/terraform-client-pactflow-application-saas.json | grep $.header
-sed -i.bak -E "s/\$\.header\./\$\.headers\./g" "$PWD/client/pacts/terraform-client-pactflow-application-saas.json"
-cat $PWD/client/pacts/terraform-client-pactflow-application-saas.json | grep $.header
+
+sed -i -E 's/\$\.header\./\$\.headers\./g' "$PWD/client/pacts/terraform-client-pactflow-application-saas.json"
 
 echo "Done..."
