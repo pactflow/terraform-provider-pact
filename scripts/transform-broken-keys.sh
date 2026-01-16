0de930f48129c74b1deb57d50f4605cc6c5128f3
Tuan Pham <tuan.pham@smartbear.com>
2026-01-16 13:45:28 +0700

    fix: update script as part of name change

--- scripts/transform-broken-keys.sh
+++ scripts/transform-broken-keys.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-sed -i -E 's/\$\.header\./\$\.headers\./g' "$PWD/client/pacts/terraform-client-pactflow-application-saas.json"
+sed -i -E 's/\$\.header\./\$\.headers\./g' "$PWD/client/pacts/terraform-client-delete-me-pactflow-application-saas.json"
 
 echo "Done..."
